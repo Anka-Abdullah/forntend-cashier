f16168e9d7951e0f16bc746468e07f6379d6789d
Anka-Abdullah <ankaabdullah.id@gmail.com>
2020-10-19 09:15:29 +0700

    cashier

--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,29 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+  path: '/',
+  name: 'Home',
+  component: Home
+},
+{
+  path: '/history',
+  name: 'History',
+  component: () =>
+    import(/* webpackChunkName: "about" */ '../views/History.vue')
+},
+{
+  path: '/cashier',
+  name: 'Cashier',
+  component: () =>
+    import(/* webpackChunkName: "about" */ '../views/Cashier.vue')
+},
+{
+  path: '/landingpage',
+  name: 'LandingPage',
+  component: () =>
+    import(/* webpackChunkName: "about" */ '../views/LandingPage.vue')
+}
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import navbar from '../components/navbarhome'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    navbar
   }
 }
 </script>
